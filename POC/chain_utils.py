068c9c7cea19de05075c94e446d4a78f79d81d98
sbyts <tornmails@gmail.com>
2024-06-19 17:44:05 +0300

    Update chain_utils.py

--- POC/chain_utils.py
+++ POC/chain_utils.py
@@ -1,62 +1,64 @@
-
 import os
+import matplotlib.pyplot as plt
+import matplotlib
 import tempfile
 from typing import List
 import pandas as pd
+from langchain_community.llms.ollama import Ollama
 from pandas import DataFrame
-from pandasai import SmartDataframe, skill
+from pandasai import SmartDataframe, Agent
+from pandasai.skills import skill
 import streamlit as st
 from pandasai.llm.local_llm import LocalLLM
 
-def get_data_from_xlsx() -> List[DataFrame]:
+
+def get_data_from_spreadsheet() -> List[DataFrame]:
     docs = []
     temp_dir = tempfile.TemporaryDirectory()
     for file in st.session_state["xlsx_docs"]:
         temp_filepath = os.path.join(temp_dir.name, file.name)
+        function = pd.read_csv if temp_filepath.endswith(".csv") else pd.read_excel
         with open(temp_filepath, "wb") as f:
             f.write(file.getvalue())
-        data = pd.read_excel(temp_filepath)
+        data = function(temp_filepath)
         docs.extend(data)
-        print(data.shape)
-        print(data.columns.tolist())
-        # TODO: plan to work with several not with one
-    return pd.read_excel(temp_filepath)
+    return data
+
 
 def get_llm() -> LocalLLM:
-    # return Ollama(model='llama3:8b-instruct-q5_K_M')
-    # return Ollama(model='phi3')
-    # return Ollama(model='llama3:8b')
-    # return LlamaCpp(model_path="./models/codellama-7b-instruct.Q4_K_M.gguf", max_tokens=2000, n_ctx=2048)
-    return LocalLLM(api_base="http://localhost:11434/v1", model="codellama")
-
+    return Ollama(model='llama3:8b-instruct-q5_K_M')
 
 
 @skill
-def plot_sales_amount(names: list[str], salaries: list[int]):
+def plot_sales_amount(sales_amount: list[int], categories: list[str]):
     """
     Displays the bar chart having name on x-axis and salaries on y-axis using streamlit
     Args:
-        names (list[str]): Employees' names
-        salaries (list[int]): Salaries
+        sales_amount (list[str]): Sales Amount
+        categories (list[int]): Categories
     """
-    import matplotlib.pyplot as plt
-
-    plt.bar(names, salaries)
-    plt.xlabel("Sales Amount")
+    print("... skills is used ... ")
+    plt.bar(sales_amount, categories)
+    plt.xlabel("Sales")
     plt.ylabel("Category")
-    plt.title("Sales amount by category")
+    plt.title("Sales amount")
     plt.xticks(rotation=45)
     plt.savefig("temp_chart.png")
     fig = plt.gcf()
     st.pyplot(fig)
+    st.session_state["current_image"] = fig
 
 
 def get_chain_by_file():
-    docs = get_data_from_xlsx()
+    docs = get_data_from_spreadsheet()
+    initial_conf = {"verbose": True, "temperature": 0, "enable_cache": False, "open_charts": False,
+                    "save_charts": True}
+    local_llm = {"llm": get_llm()}
     if os.environ['PANDASAI_API_KEY']:
-        df = SmartDataframe(docs)
-    else:
-        df = SmartDataframe(docs, config={"llm": get_llm(), "verbose": True,})
-    df.add_skills(plot_sales_amount)
-    return df
+        agent = Agent([docs], memory_size=10,
+                      config=initial_conf)
 
+    else:
+        agent = SmartDataframe(docs, config=initial_conf.update(local_llm))
+    agent.add_skills(plot_sales_amount)
+    return agent
